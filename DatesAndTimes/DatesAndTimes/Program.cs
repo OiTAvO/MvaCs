786e643b0ad446e80e63641a3cb204851e403099
otavi <otavi@DESKTOP-TRGB3M5>
2017-04-20 22:29:13 -0300

    Aula 13(trabalhando com datas e horas).

---
+++ DatesAndTimes/DatesAndTimes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myValue = DateTime.Now;
+            //Console.WriteLine(myValue.ToString());
+            //Console.WriteLine(myValue.ToShortDateString());
+            //Console.WriteLine(myValue.ToShortTimeString());
+            //Console.WriteLine(myValue.ToLongDateString());
+            //Console.WriteLine(myValue.ToLongTimeString());
+
+            //Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            //Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+            //Console.WriteLine(myValue.AddDays(-3).ToLongDateString());
+
+            //Console.WriteLine(myValue.Month);
+
+            //DateTime myBirthDay = new DateTime(1991, 12, 6);
+            //Console.WriteLine(myBirthDay.ToShortDateString());
+
+            DateTime myBirthDay = DateTime.Parse("24/05/1991");
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthDay);
+            Console.WriteLine(myAge.TotalDays);
+
+            //DateTime myBirthDay = DateTime.Parse("24/05/1991");
+            //TimeSpan myAge = DateTime.Parse("24/05/2030").Subtract(myBirthDay);
+            //Console.WriteLine(myAge.TotalDays);
+
+            Console.ReadLine();
+        }
+    }
+}
