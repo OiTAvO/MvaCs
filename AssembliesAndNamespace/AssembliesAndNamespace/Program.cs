960f8d45be3d73446356c2fdf20f20371c2e295a
OiTAvO <otavio.porto@fatec.sp.gov.br>
2017-05-05 16:47:48 -0300

    Aula 17 Add references and Use of namespaces

---
+++ AssembliesAndNamespace/AssembliesAndNamespace/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+
+            Console.WriteLine(reply);
+            File.WriteAllText(@"C:\Users\otavi\Source\Repos\MvaCs\AssembliesAndNamespace\Aula17.txt", reply);
+            Console.ReadLine();
+        }
+    }
+}
